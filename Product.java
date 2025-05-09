25685d5c03a60c3705d6204a58da8f1039a7de64
pujitha-reddy-gth <pujithathammaneni@gmail.com>
2025-05-09 20:48:07 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,19 @@
+package com.blc.elc;
+
+public class Product {
+	
+    private String productName;
+	  private int productId;
+	  private double price;
+	 
+	public void setProductDetails(String name, int id, double prc){
+	    productName = name;
+	    productId = id;
+	    price = prc;
+	  }
+	   public String getProductDetails(){
+	    return "Product Name:"+ productName + "\nProduct Id:" + productId + "\nPrice:" + price;
+	    }
+
+
+}
